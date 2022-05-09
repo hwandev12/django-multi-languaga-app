ad1cb957f2927b0148dcbd6dc2c0c33974f50747
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-09 09:52:02 +0500

    install updates

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'multilanguage'
+    'multilanguage',
+    'modeltranslation',
 ]
 
 MIDDLEWARE = [
